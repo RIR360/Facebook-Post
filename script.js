339dff25b19ae0aaf70eebd62d0cd7258e7ddd68
Rejwan Islam <rejwanislamrizvy7@gmail.com>
2021-06-05 14:30:19 +0600

    Option menu fix

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-// The power of JavaScript
-//option menu scripts
-function option_toggle(){
-	var option_button = document.getElementById("option_div");
-	option_button.classList.toggle("option_active");
+// utility
+function qs(elem) {return document.querySelector(elem);}
+function qsa(elem) {return document.querySelectorAll(elem);}
 
-}
+// globals
+var 
+menu_open = 0;
+
+// elements
+const
+opt_btns = qsa(".top-options button");
+
+// functions
+
+//******************
+// Main Function here
+//******************
+window.addEventListener('load', function() {
+
+	opt_btns.forEach(btn => {
+		btn.addEventListener("click", ()=>{
+			alert("Work in progress...");
+		});
+	});
+});
