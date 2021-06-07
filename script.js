fb290567231f4f56833abe621526cbc16765f9fd
Rejwan Islam <rejwanislamrizvy7@gmail.com>
2021-06-07 09:53:56 +0600

    comment functionality

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ menu_open = 0;
 
 // elements
 const
-opt_btns = qsa(".top-options button");
-menu = qs("#option");
+opt_btns = qsa(".top-options button"),
+menu = qs("#option"),
+comment_inputs = qsa(".comment-input-area input"),
+comment_boxs = qsa(".comment-box");
 
 // functions
 function getOffset(el) {
@@ -33,6 +35,32 @@ function toggle_option(button)
 		menu.style.left = getOffset(button).left - 315 + "px";
 	}
 }
+function makeComment(text, id) {
+	qs("#cb-" + id).innerHTML += 
+	`
+	<div class="comment-container">
+		<div class="comment">
+		<img src="assets/profile-pic.jpg" alt="" class="comment-img">
+		<div class="comment-text">
+			<div class="comment-header">
+			<p><strong>Rejwan Islam Rizvy</strong></p>
+			</div>
+			<p>${text}</p>
+		</div>
+		<div class="three-dot">
+			<img src="svg/three_dot_gray.svg" class="three-dot-img" alt="">
+		</div>
+		</div>
+		<div class="comment-lks">
+		<p>
+			<span>Like</span><span class="dot"> . </span>
+			<span>Reply</span><span class="dot"> . </span>
+			<span>Share</span><span class="dot"> . </span>
+			<span>just now</span></p>
+		</div>
+	</div>
+	`;
+}
 
 //******************
 // Main Function here
@@ -44,14 +72,12 @@ window.addEventListener('load', function() {
 			toggle_option(btn);
 		});
 	});
-	/*
-	qs("#mother").addEventListener("click", (e)=>{
-		if (menu.style.visibility == "visible")
-		{
-			menu.style.visibility = "hidden";
-		}
+	comment_inputs.forEach(input => {
+		input.addEventListener("change", (e)=>{
+			makeComment(input.value, input.getAttribute("comment-id"));
+			input.value = "";
+		});
 	});
-	*/
 });
 
 
